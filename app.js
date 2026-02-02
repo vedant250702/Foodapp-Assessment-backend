ae7eb9f6dad6749e86d9f004bd62f4ed4418e5b9
vedant250702 <vedant.vartak95@gmail.com>
2026-02-02 16:53:08 +0530

    Removing some comments

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express=require("express")
 const app=express()
 const server=require("http").createServer(app)
+const rateLimit = require("express-rate-limit");
 const cors=require("cors")
 require("dotenv").config()
+
+// RATE LIMIT MIDDLEWARE
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, 
+  max: 100,                
+  message: "Too many requests, please try again later",
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
 // DATABASE IMPORTS
 const {connectDB}=require("./app/Configs/db.config")
@@ -18,6 +28,7 @@ const PORT= process.env.PORT || 5000
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors({origin:process.env.FRONTEND_BASEURL,credentials:true}))
+app.use(limiter);
 
 //ROUTES IMPORTS
 const {itemsRouter}=require("./app/Routes/items.routes")
