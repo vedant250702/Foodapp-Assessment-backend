1627fcd1da4eb9bd5e88d5b4eac9bab5a360d690
vedant250702 <vedant.vartak95@gmail.com>
2026-02-02 16:36:26 +0530

    Removing some comments

--- app/Routes/orders.routes.js
+++ app/Routes/orders.routes.js
@@ -1,13 +1,11 @@
 const express=require("express")
 const router=express.Router()
-const {getOrders,placeOrder,getOrderById,updateOrderStatus}=require("../Controllers/orders.controller")
+const {getOrders,placeOrder,updateOrderStatus}=require("../Controllers/orders.controller")
 const {placeOrderValidator,updateOrderStatusValidator}=require("../Validators/orders.validator")
 
 router.route("/getOrders").get(getOrders)
 
 router.route("/placeOrder").post(placeOrderValidator,placeOrder)
-
-// router.get("/orders/:orderId", getOrderByIdValidator, getOrderById);
 
 router.patch("/:orderId/status", updateOrderStatusValidator, updateOrderStatus);
 
